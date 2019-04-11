dbe3e04c1da5416e96651e3eea30591ab87fab1b
Marcin Mucha <marcin485@op.pl>
2019-04-11 19:59:45 +0200

    File monitor functionality

--- FileChecker.h
+++ FileChecker.h
@@ -7,15 +7,59 @@
 #include <chrono>
 #include <string>
 #include <functional>
+#include <experimental/algorithm>
+
 
 enum class State {Created, Modified, Removed};
 using namespace std::experimental;
 
 class FileChecker{
-    FileChecker(const std::string& file_path, std::chrono::duration<int, std::milli> delay)
 private:
     std::unordered_map<std::string, filesystem::file_time_type> paths_;
     std::chrono::duration<int, std::milli> delay_;
+    std::string main_path;
+
+    bool contains(const std::string& file_name){
+        return std::find(paths_.begin(), paths_.end(), file_name) != paths_.end();
+    }
+public:
+    FileChecker(const std::string& file_path, std::chrono::duration<int, std::milli> delay): main_path(file_path),
+        delay_(delay){
+        for(auto& f: filesystem::recursive_directory_iterator(main_path))
+            paths_[f.path().string()] = filesystem::last_write_time(f);     // add path as a key, and time data as a value
+    }
+
+    void startChecking(const std::function<void(std::string, State)>& validate){
+        while(true){  // infinity loop
+            std::this_thread::sleep_for(delay_);    // set refresh every "delay_" seconds
+
+            // checking if one of the files was erased
+            for(auto&[path, time]: paths_){
+                if(!filesystem::exists(path)){
+                    validate(path, State::Removed);
+                    paths_.erase(path);             // delete erased path
+                }
+            }
+
+            for(auto& file: filesystem::recursive_directory_iterator(main_path)){
+                auto lastWriteTime(filesystem::last_write_time(file));
+
+                    // check if file was created
+                if(auto s(file.path().string()); !contains(s)){
+                    paths_[s] = lastWriteTime;
+                    validate(s,State::Created);
+                }else{
+                    // check if file was modificated
+                    if(paths_[s] != lastWriteTime){
+                        paths_[s] = lastWriteTime;
+                        validate(s,State::Modified);
+                    }
+                }
+            }
+
+        }
+    }
+
 };
 
 #endif //FILECHECKER_FILECHECKER_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "FileChecker.h"
-#include <experimental/filesystem>
-#include <thread>
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
