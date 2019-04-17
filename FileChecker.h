87543d83d49104c38363297c91ae28eda1c4e41c
Marcin Mucha <marcin485@op.pl>
2019-04-18 01:02:07 +0200

    Add save to file feature

--- FileChecker.h
+++ FileChecker.h
@@ -7,7 +7,8 @@
 #include <chrono>
 #include <string>
 #include <functional>
-
+#include <fstream>
+#include <optional>
 
 enum class State {Created, Modified, Removed};
 using namespace std::experimental;
@@ -17,53 +18,91 @@ private:
     std::unordered_map<std::string, filesystem::file_time_type> paths_;
     std::chrono::duration<int, std::milli> delay_;
     std::string main_path;
+    std::optional<std::ofstream> alert_file;
+    std::string fileToSave;
+    bool ifSave;
 
     bool contains(const std::string& file_name){
         // return std::find_if(paths_.begin(), paths_.end(), [file_name](const auto& pair){
         // return pair.first == file_name;}) != paths_.end();
         return paths_.find(file_name) != paths_.end();
     }
+
+    void saveToFile(const std::string& file_name, const std::pair<filesystem::path, time_t>& info){
+
+        try{
+            if(!alert_file) { // if alert_file is std::nullopt
+                alert_file.emplace(file_name, std::ios::app);    // default is open
+            }else
+                alert_file->open(file_name, std::ios::app);
+
+            if (alert_file->is_open()) {
+                *alert_file <<" +  " << info.first << "\t\t" << std::asctime(std::localtime(&info.second))<<"\n";
+                //*alert_file << "Marcin Mucha\n";
+            }
+
+            alert_file->close();
+        }
+        catch(std::ostream::failure e){
+            std::cout << e.what();
+            alert_file->close();
+            alert_file.reset();
+
+        }
+    }
 public:
-    FileChecker(const std::string& file_path, std::chrono::duration<int, std::milli> delay): main_path(file_path),
-        delay_(delay){
+    using time = std::chrono::system_clock;
+
+    FileChecker(const std::string& file_path, std::chrono::duration<int, std::milli> delay, bool saveToFile = false): main_path(file_path),
+        delay_(delay), alert_file(std::nullopt), ifSave(saveToFile){
+
+        fileToSave = ifSave ? "changes.txt" : "";
+
         for(auto& f: filesystem::recursive_directory_iterator(main_path))
             paths_[f.path().string()] = filesystem::last_write_time(f);     // add path as a key, and time data as a value
     }
 
     void startChecking(const std::function<void(const std::string&, State)>& validate){
 
-        while(true){  // infinity loop
-            std::this_thread::sleep_for(delay_);    // set refresh every "delay_" seconds
+        while(true){                                                 // infinity loop
+            std::this_thread::sleep_for(delay_);                     // set refresh every "delay_" seconds
 
-            // checking if one of the files was erased
-            auto it(paths_.begin());
-            for(;it!=paths_.end();){
-              if(!filesystem::exists(it->first)){
-                  validate(it->first, State::Removed);
-                  paths_.erase(it++);
-              }
-              else it++;
+            for(auto it = paths_.begin(); it != paths_.end();){      // checking if one of the files was erased
+                auto [file, mod_data] = *it;
+
+                if(!filesystem::exists(file)){
+                    validate(file, State::Removed);
+                    if(ifSave) saveToFile(fileToSave, std::make_pair(file, time::to_time_t(mod_data)));
+                    paths_.erase(it++);
+                } else it++;
             }
-
 
             for(auto& file: filesystem::recursive_directory_iterator(main_path)){
                 auto lastWriteTime(filesystem::last_write_time(file));
 
-                    // check if file was created
-                if(auto s(file.path().string()); !contains(s)){
-                    paths_[s] = lastWriteTime;
+                //saveToFile("changes.txt", std::make_pair(file.path().string(), std::chrono::system_clock::to_time_t(lastWriteTime)));
+
+                if(auto s(file.path().string()); !contains(s)){     // check if file was created
+                    paths_.insert({s,lastWriteTime});
+
+                    if(ifSave)
+                        saveToFile(fileToSave, std::make_pair(file.path().string(),time::to_time_t(lastWriteTime)));
+
                     validate(s,State::Created);
-                }else{
-                    // check if file was modificated
+                }else{                                              // check if file was modificated
                     if(paths_[s] != lastWriteTime){
                         paths_[s] = lastWriteTime;
+
+                        if(auto p(filesystem::current_path()/fileToSave); ifSave && s != p.string())
+                            saveToFile(fileToSave, std::make_pair(file.path().string(),time::to_time_t(lastWriteTime)));
+
                         validate(s,State::Modified);
                     }
                 }
             }
-
         }
     }
+
 };
 
 #endif //FILECHECKER_FILECHECKER_H
--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 #include "FileChecker.h"
 #include <algorithm>
 
+// EXAMPLE
+
+//        File created: C:\Users\Marcin\CLionProjects\FileChecker\cmake-build-debug\Nowy dokument tekstowy.txt
+//        File removed: C:\Users\Marcin\CLionProjects\FileChecker\cmake-build-debug\Nowy dokument tekstowy.txt
+//        File created: C:\Users\Marcin\CLionProjects\FileChecker\cmake-build-debug\poq.txt
+//        File modified: C:\Users\Marcin\CLionProjects\FileChecker\cmake-build-debug\poq.txt
+//        File removed: C:\Users\Marcin\CLionProjects\FileChecker\cmake-build-debug\poq.txt
+
 int main() {
 
 using namespace std::experimental;
+
+
     auto delay(std::chrono::milliseconds(5000));
-    FileChecker monit{filesystem::current_path().string(), delay};
+
+    FileChecker monit{filesystem::current_path().string(), delay, true};
 
     auto disp_Alert([](const std::string& path_, State type_of_change)-> void{
 
-        if(!filesystem::is_regular_file(filesystem::path(path_)) && type_of_change != State::Removed){
-            return;
-        }
+//        if(!filesystem::is_regular_file(filesystem::path(path_)) && type_of_change != State::Removed){
+//            return;
+//        }
 
         switch(type_of_change){
             case State::Created:
@@ -30,9 +41,6 @@ using namespace std::experimental;
     });
 
     monit.startChecking(disp_Alert);
-
-
-
 
     return 0;
 }
